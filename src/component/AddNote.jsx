56f4b49df1801e724b096446b3b914664ae2fc4d
va1shakh <vaishakhvgd@gmail.com>
2026-07-30 18:44:26 +0530

    fonts added

--- src/component/AddNote.jsx
+++ src/component/AddNote.jsx
@@ -24,7 +24,7 @@ function AddNote({addNote, setIsOpen}){
         <div className="bg-[#a6138d] p-4 rounded-xl flex flex-col shadow-lg gap-3"> {/* card */}
             <div className="flex flex-col gap-2">{/*title and notes*/}
                 <input 
-                    className="p-1 py-2 rounded-lx border-none outline-none text-white placeholder-white text-lg" 
+                    className="font-outfit tracking-wider font-medium text-2xl p-1 py-2 rounded-lx border-none outline-none text-white placeholder-white" 
                     type="text" 
                     placeholder="Enter title..." 
                     maxLength={40}
@@ -32,7 +32,7 @@ function AddNote({addNote, setIsOpen}){
                     onChange={handleTitle}
                 />
                 <textarea 
-                    className="p-2 rounded-lg h-30 bg-[#c33ead] outline-none resize-none text-white placeholder-white shadow-sm" 
+                    className="font-inter text-lg p-2 rounded-lg h-30 bg-[#c33ead] outline-none resize-none text-white placeholder-white shadow-sm" 
                     placeholder="Enter note..." 
                     maxLength={200}
                     value={noteText}
@@ -40,12 +40,12 @@ function AddNote({addNote, setIsOpen}){
                 />
             </div>
             <div className="flex items-center justify-between p-2 text-white"> {/*remaining and save button*/}
-                <small className="text-sm">{remaining} remaining</small>
-                <button className="bg-white text-[#560045] p-2 px-4 rounded-lg hover:bg-[#fef0fb] cursor-pointer text-lg shadow-lg flex justify-between gap-2" onClick={() => setIsOpen(true)}>
+                <small className="text-sm font-inter">{remaining} remaining</small>
+                <button className="font-outfit bg-white text-[#560045] p-2 px-4 rounded-lg hover:bg-[#fef0fb] cursor-pointer text-xl shadow-lg flex justify-between gap-2" onClick={() => setIsOpen(true)}>
                     color<Palette />
                 </button>
                 <button 
-                    className="bg-white text-[#560045] p-2 px-4 rounded-lg hover:bg-[#fef0fb] cursor-pointer text-lg shadow-lg" onClick={handleSave}>
+                    className="bg-white text-[#560045] p-2 px-4 rounded-lg hover:bg-[#fef0fb] cursor-pointer text-xl font-outfit shadow-lg" onClick={handleSave}>
                     save
                 </button>
             </div>
--- src/component/Note.jsx
+++ src/component/Note.jsx
@@ -3,10 +3,10 @@ import { Trash2 } from "lucide-react";
 function Note({note, dltNote}) {
   return (
     <div style={{backgroundColor: note.color}} className=" p-4 rounded-xl gap-5 flex flex-col justify-between shadow-lg"> {/* Note card */}
-      <h2>{note.title}</h2> {/* Title */}
-      <p className="whitespace-normal break-word">{note.text}</p> {/* notes */}
+      <h2 className="font-outfit capitalize font-medium tracking-wider text-2xl">{note.title}</h2> {/* Title */}
+      <p className="font-inter text-lg whitespace-normal break-word">{note.text}</p> {/* notes */}
       <div className="flex items-center justify-between"> {/* date and delete button */}
-        <small className="date">{note.date}</small>
+        <small className="font-inter date">{note.date}</small>
         <button className="cursor-pointer hover:text-[#804077]" onClick={() => dltNote(note.id)}>
           <Trash2 />
         </button>
